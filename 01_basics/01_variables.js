96e22fcb48e1370569cea3d6b6fa923c55e7d6cd
Soumyadip Maity <maitysoumya904@gmail.com>
2025-07-30 18:02:32 +0530

    Create 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "soumyadip@google.com"
+var accountPassword = "67890"
+accountCity = "Contai"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "sm@sm.com"
+accountPassword = "21212121"
+accountCity = "Kolkata"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
